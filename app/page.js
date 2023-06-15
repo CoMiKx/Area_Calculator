b0acaf7ae7cb171a77c098221349da8c2e38e3ed
CoMiKx <su.baramee_st@tni.ac.th>
2023-06-15 15:15:04 +0900

    change app

--- app/page.js
+++ app/page.js
@@ -1,12 +1,40 @@
+"use client";
 import Image from 'next/image'
+import { useState } from 'react';
 
 export default function Home() {
+  const [area, setArea] = useState('');
+  const [unit, setUnit] = useState('');
+  const [result, setResult] = useState(null);
+
+  const handleCalculate = () => {
+    // Perform the calculation logic here
+    // You can access the entered area value from the 'area' state
+    // and the selected unit value from the 'unit' state
+
+    // Convert the area to square meters
+    let areaInSquareMeters = area;
+    if (unit === 'option3') {
+      areaInSquareMeters *= 400;
+    } else if (unit === 'option2') {
+      areaInSquareMeters *= 100;
+    } else if (unit === 'option1') {
+      areaInSquareMeters *= 1;
+    }
+
+    // Convert the area to rai
+    const areaInRai = areaInSquareMeters / 1600;
+
+    // Update the 'result' state with the calculated value in rai
+    setResult(areaInRai);
+  };
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+          Develop and editing by&nbsp;
+          <code className="font-mono font-bold">CoMiKx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -39,74 +67,56 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left w-3/4">
+        <form className="text-center">
+          <label htmlFor="area" className="block mb-2 w-auto">
+            ใส่พื้นที่
+          </label>
+          <input
+            type="number"
+            id="area"
+            className="text-black w-1/2"
+            value={area}
+            onChange={(e) => setArea(e.target.value)}
+          /><br /><br />
+        </form>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <form className="text-center">
+          <label htmlFor="unit" className="block mb-2">
+            เลือกหน่วย
+          </label>
+          <select
+            id="unit"
+            className="text-black w-1/2"
+            value={unit}
+            onChange={(e) => setUnit(e.target.value)}
+          >
+            <option value="option1">ตร.ม</option>
+            <option value="option2">ตร.วา</option>
+            <option value="option3">งาน</option>
+          </select>
+        </form>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <form>
+          <div className="text-center">
+            <button
+              type="button"
+              className="mt-4 px-4 py-2 bg-green-300 text-black"
+              onClick={handleCalculate}
+            >
+              Calculate
+            </button>
+          </div>
+        </form>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="text-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+            Result
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className={`m-0 max-w-[30ch] text-sm opacity-75 result`}>
+            {result !== null ? `The result is: ${result} ไร่` : 'Result will appear here'}
           </p>
-        </a>
+        </div>
       </div>
     </main>
   )
